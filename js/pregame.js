5c4f84e63660451345a31b41aac1539c91997c26
PhilMayer <phillipmmayer@gmail.com>
2017-03-23 17:50:47 -0400

    Refactor game over animations with promises

--- js/pregame.js
+++ js/pregame.js
@@ -78,52 +78,49 @@ const drawLevel = (text, color) => {
 export const gameOver = (stage, musicPlayer, scoreboard, hits, misses, run, tempo) => {
   const applause = "applause";
   createjs.Sound.registerSound("assets/applause.mp3", applause);
+  const message = new createjs.Text("You're basically Beethoven.", "30px Reenie Beanie");
+  const githubLink = new createjs.Text("A game by Phil Mayer => Github", "40px Reenie Beanie")
+  const playAgain = new createjs.Text("Play again!!", "80px Reenie Beanie", "#00FF00")
 
-  setTimeout(() => {
+  delay(2000).then(() => {
     musicPlayer.stopMusic();
     createjs.Tween.get(scoreboard).to({x: 75, y: 500, rotation: -360},
       700, createjs.Ease.bounceOut);
+    return delay(1000);
+  }).then(() => {
+    createjs.Sound.play(applause);
+    message.x = 75;
+    message.y = 450;
+    stage.addChild(message);
+    return delay(1000);
+  }).then(() => {
+    githubLink.addEventListener("click", () => window.open("https://github.com/PhilMayer/JSHero"))
+    githubLink.cursor = "pointer";
+    githubLink.x = 75;
+    githubLink.y = 20;
 
-    setTimeout(() => {
-      createjs.Sound.play(applause);
-      const message = new createjs.Text("You're basically Beethoven.", "30px Reenie Beanie");
-      message.x = 75;
-      message.y = 450;
-      stage.addChild(message);
+    const githubClick = new createjs.Shape();
+    githubClick.graphics.beginFill("#000").drawRect(0, 0, githubLink.getMeasuredWidth(), githubLink.getMeasuredHeight());
+    githubLink.hitArea = githubClick;
 
-      setTimeout(() => {
-        const githubLink = new createjs.Text("A game by Phil Mayer => Github", "40px Reenie Beanie")
-        githubLink.addEventListener("click", () => window.open("https://github.com/PhilMayer/JSHero"))
-        githubLink.cursor = "pointer";
-        githubLink.x = 75;
-        githubLink.y = 20;
+    stage.addChild(githubLink);
+    return delay(200);
+  }).then(() => {
+    playAgain.addEventListener("click", () => {
+      stage.removeChild(playAgain, githubLink, message, scoreboard)
+      hits = 0;
+      misses = 0;
+      run(tempo);
+    })
+    playAgain.cursor = "pointer";
+    playAgain.x = 75;
+    playAgain.y = 100;
 
-        const githubClick = new createjs.Shape();
-        githubClick.graphics.beginFill("#000").drawRect(0, 0, githubLink.getMeasuredWidth(), githubLink.getMeasuredHeight());
-        githubLink.hitArea = githubClick;
-
-        stage.addChild(githubLink);
-
-        setTimeout(() => {
-          const playAgain = new createjs.Text("Play again!!", "80px Reenie Beanie", "#00FF00")
-          playAgain.addEventListener("click", () => {
-            stage.removeChild(playAgain, githubLink, message, scoreboard)
-            hits = 0;
-            misses = 0;
-            run(tempo);
-          })
-          playAgain.cursor = "pointer";
-          playAgain.x = 75;
-          playAgain.y = 100;
-
-          const hit = new createjs.Shape();
-          hit.graphics.beginFill("#000").drawRect(0, 0, playAgain.getMeasuredWidth(), playAgain.getMeasuredHeight());
-          playAgain.hitArea = hit;
-          stage.addChild(playAgain);
-        }, 200)
-      }, 1000)
-    }, 1000)
-  }, 2000)
+    const hit = new createjs.Shape();
+    hit.graphics.beginFill("#000").drawRect(0, 0, playAgain.getMeasuredWidth(), playAgain.getMeasuredHeight());
+    playAgain.hitArea = hit;
+    stage.addChild(playAgain);
+  })
 }
 
 export const selectLevel = (stage, run) => {
